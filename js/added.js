14d73933f8a9b846162163d9272a965f0fddc26d
Phat X Luong <66936482+phatx88@users.noreply.github.com>
2020-10-19 14:12:12 +0700

    main

---
+++ js/added.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function () {
+    const loader = document.querySelector(".loader");
+    setInterval(function(){
+        loader.className += " hidden"; // class "loader hidden"
+    },10000);
+});
