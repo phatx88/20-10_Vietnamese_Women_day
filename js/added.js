eb381138db3d33295d9895addfa14a311c571982
Phat X Luong <66936482+phatx88@users.noreply.github.com>
2020-10-20 02:27:03 +0700

    add files

--- js/added.js
+++ js/added.js
@@ -1,7 +1,27 @@
-window.addEventListener("load", function () {
+window.addEventListener("load", function () {   
     const loader = document.querySelector(".loader");
-    setInterval(function(){
+    setInterval(function(){       
         loader.className += " hidden"; // class "loader hidden"
     },10000);
+    
+});
+
+const button = document.querySelector("#button");
+const icon = document.querySelector("#button > i");
+const audio = document.querySelector("audio");
+
+button.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.volume = 0.8;
+    audio.play();
+    icon.classList.remove('fa-volume-up');
+    icon.classList.add('fa-volume-mute');
+    
+  } else {
+    audio.pause();
+    icon.classList.remove('fa-volume-mute');
+    icon.classList.add('fa-volume-up');
+  }
+  button.classList.add("fade");
 });
 
